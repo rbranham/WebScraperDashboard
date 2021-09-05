814c5a323b13478f27e2db6f309a3478382a317d
Roger Branham <roger.brnhm@gmail.com>
2021-09-05 14:26:15 -0500

    Function to Replace team IDs with their names in stats list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json;
-
+import pandas as pd
 
 def loadTeams():
     """ Function to load teams, currently pulls from mock data, eventually could pull from an API"""
@@ -10,11 +10,19 @@ def loadConferenceStats(confId: int):
     """ Function to load stats, currently returns mock data"""
     return json.load(open('sample_json/conference_one.json'))
 
+def replaceTeamID(statDF, teamDF):
+    """ Function to replace team ids with their name"""
+    statDF['teamId'] = statDF['teamId'].replace(dict(zip(teamDF.id, teamDF.teamName)))
+    return statDF
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    for stat in loadConferenceStats(1):
-        print(stat)
+    """Main function"""
+    statsList = pd.DataFrame.from_dict(loadConferenceStats(1))
+    teamList = pd.DataFrame.from_dict(loadTeams())
+
+    statsList = replaceTeamID(statsList, teamList)
+    print(statsList)
 
 
     # teams = loadTeams()
