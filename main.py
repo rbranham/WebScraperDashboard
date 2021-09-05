fe1b9474e6d9d24bae9cde31ae7d48fd8aea5296
Roger Branham <roger.brnhm@gmail.com>
2021-09-05 18:00:53 -0500

    Added dash app functionality, with simple line graph by win percentage

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import json
 import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.express as px
 
 def loadTeams():
     """ Function to load teams, currently pulls from mock data, eventually could pull from an API"""
@@ -28,18 +33,44 @@ def loadConferenceStats(confId: int):
     return json.load(open('sample_json/conference_one.json'))
 
 
+def addPercentage(statDF):
+    """ Function to add column with win percentage"""
+    statDF['win_pct'] = statDF['wins'] / (statDF['wins'] + statDF['losses'])
+    return statDF
+
+
+
+
+# Set up dataframe
+statsDF = pd.DataFrame.from_dict(loadConferenceStats(1))
+teamList = pd.DataFrame.from_dict(loadTeams())
+seasonList = pd.DataFrame.from_dict(loadSeasons())
+
+statsDF = replaceTeamID(statsDF, teamList)
+statsDF = replaceSeasonId(statsDF, seasonList)
+statsDF = addPercentage(statsDF)
+
+statsDF = statsDF.sort_values(by='seasonId')
+
+########################### Dash Code #####################################
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+fig = px.line(statsDF, x="seasonId", y="win_pct", color="teamId")
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='line-graph',
+        figure=fig
+    )
+])
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     """Main function"""
-    statsList = pd.DataFrame.from_dict(loadConferenceStats(1))
-    teamList = pd.DataFrame.from_dict(loadTeams())
-    seasonList = pd.DataFrame.from_dict(loadSeasons())
-    # print(seasonList)
-
-    statsList = replaceTeamID(statsList, teamList)
-    statsList = replaceSeasonId(statsList, seasonList)
-    print(statsList)
-
+    print(statsDF)
+    app.run_server(debug=True)
 
     # teams = loadTeams()
     # for team in teams:
